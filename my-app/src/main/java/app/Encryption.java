9585b700c4f5f746b3be944aaf5140d8cff9eab0
Conor <conoroheocha@gmail.com>
2020-04-06 19:21:37 +0100

    Remover underscore and base65 util

--- my-app/src/main/java/app/Encryption.java
+++ my-app/src/main/java/app/Encryption.java
@@ -1,7 +1,6 @@
 package app;
 
 import java.security.GeneralSecurityException;
-import java.util.Base64;
 
 import com.google.crypto.tink.Aead;
 import com.google.crypto.tink.KeysetHandle;
@@ -12,30 +11,30 @@ import com.google.crypto.tink.config.TinkConfig;
 public class Encryption {
 	private static Aead aead;
 
-	Encryption() throws GeneralSecurityException{
-	    TinkConfig.register();
-		
-	    KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES128_GCM);
-	    aead = AeadFactory.getPrimitive(keysetHandle);
+	Encryption() throws GeneralSecurityException {
+		TinkConfig.register();
+
+		KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES128_GCM);
+		aead = AeadFactory.getPrimitive(keysetHandle);
 	}
-	
+
 	byte[] encrypt(String toEncrypt) throws GeneralSecurityException {
-		//System.out.println("String to Encrypt: " + toEncrypt);
+		// System.out.println("String to Encrypt: " + toEncrypt);
 		byte[] toEncryptBytes = toEncrypt.getBytes();
-		//System.out.println("Bytes to Encrypt: " + toEncryptBytes);
-	    byte[] ciphertext = aead.encrypt(toEncryptBytes, null);
-	    //System.out.println("Encrypted Bytes: " + ciphertext);
-	    
+		// System.out.println("Bytes to Encrypt: " + toEncryptBytes);
+		byte[] ciphertext = aead.encrypt(toEncryptBytes, null);
+		// System.out.println("Encrypted Bytes: " + ciphertext);
+
 		return ciphertext;
 	}
-	
+
 	String decrypt(byte[] toDecrypt) throws GeneralSecurityException {
-		//System.out.println("Bytes to Decrypt: " + toDecrypt);
+		// System.out.println("Bytes to Decrypt: " + toDecrypt);
 		byte[] decrypted = aead.decrypt(toDecrypt, null);
-		//System.out.println("Decrypted Bytes: " + decrypted);
-	    String decryptedString = new String(decrypted);
-	    //System.out.println("Decrypted String: " + decryptedString);
-	    
+		// System.out.println("Decrypted Bytes: " + decrypted);
+		String decryptedString = new String(decrypted);
+		// System.out.println("Decrypted String: " + decryptedString);
+
 		return decryptedString;
 	}
 }
